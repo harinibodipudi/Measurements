214dec2fd900129672ee94ecbbac1a062b102991
naaem <naaem@gmail.com>
2021-04-12 17:05:26 +0530

    initial commit

---
+++ src/test/java/utility/MeasurementTest.java
@@ -0,0 +1,18 @@
+package utility;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+public class MeasurementTest {
+    @Test
+    void testIf1cmIsEqualTo1cm() {
+        Measurement centimeter1 = new Measurement(1,"cm");
+        Measurement centimeter2 = new Measurement(1,"cm");
+
+        assertTrue(centimeter1.equals(centimeter2));
+    }
+
+    
+}
