ab7ce5b0e113100bcf700fa6c3fe758b75384420
naaem <naaem@gmail.com>
2021-04-12 17:33:57 +0530

    Implemented equals method to check 1cm is equal to 1 cm

--- src/main/java/utility/Measurement.java
+++ src/main/java/utility/Measurement.java
@@ -4,5 +4,19 @@ public class Measurement {
 
 
 
+    int value;
+    String units;
+
+    public Measurement(int value,String units) {
+        this.value = value;
+        this.units = units;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        Measurement measure = (Measurement) obj;
+        return measure.units == this.units && measure.value == this.value;
+    }
+
 
 }
