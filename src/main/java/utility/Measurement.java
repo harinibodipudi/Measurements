2b6550e7b1b3dc2a361e9cb09ff41ad2d06f1b7c
harinimanu <harinimanu123@gmail.com>
2021-04-16 07:58:38 +0530

    implemented all methods with generics

--- src/main/java/utility/Measurement.java
+++ src/main/java/utility/Measurement.java
@@ -2,6 +2,14 @@ package utility;
 
 public class Measurement<T extends Measurement> {
 
+    public boolean equals(T measurement1, T measurement2){
+        if(measurement1.getClass()== Length.class && measurement2.getClass()==Length.class){
+            return ((Length)measurement1).equals((Length) measurement2);
+        }else{
+            return ((Weight)measurement1).equals((Weight) measurement2);
+        }
+    }
+
 
     public T add(T measurement1, T measurement2,String expectedMetric){
            if(measurement1.getClass()== Length.class && measurement2.getClass()==Length.class){
@@ -10,4 +18,13 @@ public class Measurement<T extends Measurement> {
                return (T)((Weight)measurement1).add((Weight) measurement2,expectedMetric);
            }
        }
+
+    public T subtract(T measurement1, T measurement2,String expectedMetric){
+        if(measurement1.getClass()== Length.class && measurement2.getClass()==Length.class){
+            return (T)((Length)measurement1).subtract((Length) measurement2,expectedMetric);
+        }else{
+            return (T)((Weight)measurement1).subtract((Weight) measurement2,expectedMetric);
+        }
+    }
+
 }
--- src/test/java/utility/WeightTest.java
+++ src/test/java/utility/WeightTest.java
@@ -6,25 +6,35 @@ import org.junit.jupiter.api.Test;
 public class WeightTest {
     @Test
     void testIf1GramIsEqualTo1Gram() {
-       Assertions.assertTrue(new Weight(1,"g").equals(new Weight(1,"g")));
+       Measurement<Weight> weight = new Measurement<Weight>();
+       Assertions.assertTrue(weight.equals(new Weight(1,"g"),new Weight(1,"g"))
+       );
     }
 
     @Test
     void testIf0_1KilogramIsEqualTo100Grams() {
-        Assertions.assertTrue(new Weight(0.1,"kg").equals(new Weight(100,"g")));
+        Measurement<Weight> weight = new Measurement<Weight>();
+        Assertions.assertTrue(weight.equals(new Weight(0.1,"kg"),new Weight(100,"g"))
+        );
     }
+
+
 
     @Test
     void testIf10GramsPlus1KiloGramIsEqualTo1010Grams() {
-        Assertions.assertTrue((new Weight(10,"g")
-                        .add(new Weight(1,"kg"),"g"))
-                .equals(new Weight(1010,"g")));
+        Measurement<Weight> weight = new Measurement<Weight>();
+        Weight expectedValue = new Weight(10,"g").add(new Weight(1,"kg"),"kg");
+        Weight actualValue = weight.add(new Weight(10,"g"),new Weight(1,"kg"),"kg");
+
+        Assertions.assertTrue(expectedValue.equals(actualValue));
     }
 
     @Test
     void testIf1_5KiloGramsMinus500GramsIsEqualTo1KiloGrams() {
-        Assertions.assertTrue((new Weight(1.5,"kg")
-                .subtract(new Weight(500,"g"),"kg"))
-                .equals(new Weight(1,"kg")));
+        Measurement<Weight> weight = new Measurement<Weight>();
+        Weight expectedValue = new Weight(1.5,"kg").subtract(new Weight(500,"g"),"kg");
+        Weight actualValue = weight.subtract(new Weight(1.5,"kg"),new Weight(500,"g"),"kg");
+
+        Assertions.assertTrue(expectedValue.equals(actualValue));
     }
 }
